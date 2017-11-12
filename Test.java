dd0df0a92d2285119284f40b841fd1c1a49e4403
xieqing01 <369869314@qq.com>
2017-11-12 23:36:20 +0800

    创建了一个Test.java,并提交到远程仓库中

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+  public static void main(String[] args){
+     final String url = "jdbc:mysql://localhost:3306/crm";
+     final String userName = "root";
+     final String passWord = "123";
+  }
+}
